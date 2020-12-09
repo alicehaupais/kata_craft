481266e9eb5d7b6d130fe27a43f7929270c4b864
Alice Haupais <alice.haupais@accenture.com>
2020-12-09 16:06:37 +0100

    add empty command test

--- src/test/java/com/megacorp/ecs/CLITest.java
+++ src/test/java/com/megacorp/ecs/CLITest.java
@@ -89,4 +89,45 @@ public class CLITest {
                 "--------------------------------------------------------------------------------\n" +
                 "\n");
     }
+
+    @Test
+    public void empty_command() {
+        // Given
+        String[] args = new String[0];
+        String myString = "order\nquit\nstop\nquit\n";
+        InputStream is = new ByteArrayInputStream(myString.getBytes());
+
+        System.setIn(is);
+
+        // When
+        CLI.main(args);
+
+        content = buffer.toString();
+
+        // Then
+        assertThat(content).isEqualToNormalizingNewlines("\n" +
+                "--------------------------------------------------------------------------------\n" +
+                "          Welcome to Efficent Command System 2.0\n" +
+                "--------------------------------------------------------------------------------\n" +
+                "\n" +
+                "Enter command : \n" +
+                "--------------------------------------------------------------------------------\n" +
+                "          Order Menu\n" +
+                "--------------------------------------------------------------------------------\n" +
+                "\n" +
+                "New order created.\n" +
+                "Add new elements to your order\n" +
+                "\n" +
+                "What do you want to add to order: \n" +
+                "Nothing added\n" +
+                "\n" +
+                "Enter order command : \n" +
+                "Quit Order Menu\n" +
+                "\n" +
+                "Enter command : \n" +
+                "--------------------------------------------------------------------------------\n" +
+                "          System stopped\n" +
+                "--------------------------------------------------------------------------------\n" +
+                "\n");
+    }
 }
